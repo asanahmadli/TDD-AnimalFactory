adf1f2d1bbb2f101b3d18319df5091d5572c1e57
asanahmadli <asenahmedli@gmail.com>
2024-03-09 16:21:59 -0500

    I completed CatTest

--- src/main/java/rocks/zipcodewilmington/Food.java
+++ src/main/java/rocks/zipcodewilmington/Food.java
@@ -4,4 +4,11 @@ package rocks.zipcodewilmington;
  * @author leon on 4/19/18.
  */
 public class Food {
+
+    public String food;
+
+    public Food(String food){
+        this.food = food;
+    }
+
 }
