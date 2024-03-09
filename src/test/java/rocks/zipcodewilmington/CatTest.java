6b7dfcd1110e0dba561d9c9b81d09645e1ee8b72
asanahmadli <asenahmedli@gmail.com>
2024-03-09 16:36:24 -0500

    I completed DogTest

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -2,9 +2,14 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Mammal;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author leon on 4/19/18.
@@ -39,5 +44,70 @@ public class CatTest {
         Assert.assertEquals(givenBirthDate, retrievedBirthDate);
         Assert.assertEquals(givenId, retrievedId);
     }
+    Cat cat = new Cat("elvin", new Date(1992, 12, 30), 30);
+
+    @Test
+    public void set_Name() {
+        String expected = "nick";
+        cat.setName(expected);
+
+        String actual = cat.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void set_Birthdate() {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            Date expected = simpleDateFormat.parse("1992-30-12");
+            cat.setBirthDate(expected);
+            Date actual = cat.getBirthDate();
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void speak() {
+        String expected = "meow!";
+        String actual = cat.speak();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void eat(){
+        int expected = 1;
+        cat.eat(new Food("bone"));
+        int actual = cat.getNumberOfMealsEaten();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getId(){
+        int expected = 30;
+        int actual = cat.getId();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void instanceOfAnimal(){
+        Animal mammal = cat;
+        boolean expected = true;
+        boolean actual = mammal instanceof Animal;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void instanceOfMammal(){
+        Mammal mammal = cat;
+        boolean expected = true;
+        boolean actual = mammal instanceof Mammal;
+        assertEquals(expected,actual);
+    }
+
+
 
 }
--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -2,7 +2,15 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author leon on 4/19/18.
@@ -27,5 +35,69 @@ public class DogTest {
         // Then (we expect to get the given name from the dog)
         String dogName = dog.getName();
         Assert.assertEquals(dogName, givenName);
+    }
+
+    Dog dog = new Dog("cek", new Date(2000, 12, 30), 5);
+
+    @Test
+    public void set_Name() {
+        String expected = "marsel";
+        dog.setName(expected);
+
+        String actual = dog.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void set_Birthdate() {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            Date expected = simpleDateFormat.parse("2001-20-12");
+            dog.setBirthDate(expected);
+            Date actual = dog.getBirthDate();
+            assertEquals(expected, actual);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void speak() {
+        String expected = "bark!";
+        String actual = dog.speak();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void eat(){
+        int expected = 1;
+        dog.eat(new Food("bone"));
+        int actual = dog.getNumberOfMealsEaten();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getId(){
+        int expected = 5;
+        int actual = dog.getId();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void instanceOfAnimal(){
+        Animal mammal = dog;
+        boolean expected = true;
+        boolean actual = mammal instanceof Animal;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void instanceOfMammal(){
+        Mammal mammal = dog;
+        boolean expected = true;
+        boolean actual = mammal instanceof Mammal;
+        assertEquals(expected,actual);
     }
 }
